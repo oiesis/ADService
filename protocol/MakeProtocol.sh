a65b9b98d54d12af4a392bad161ad10e7720beca
linguoze <linguoze@mtty.com>
2016-01-28 15:58:00 +0800

    modified MakeProtocol.sh

---
+++ protocol/MakeProtocol.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+verbose=0
+
+if [ $# -eq 1 ];
+then
+	if [ $1 = 'clean' ];
+	then
+		rm -rf */*.pb.*
+		exit 0
+	elif [ $1 = '--verbose' ];
+	then
+		verbose=1	
+	fi
+fi
+
+for src_dir in `find * -maxdepth 0 -type d`
+do
+	dst_dir="$src_dir"
+	error_msg=$(protoc -I="$src_dir" --cpp_out="$dst_dir" "$src_dir"/*.proto 2>&1)
+	if [[ $? != 0 && ${verbose} == 1 ]];
+	then
+		echo "error occured processing ${src_dir}:${error_msg}"
+	fi
+done
